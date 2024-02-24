363e5e73e45a8857cb23f6376bfaf1d6d4b77572
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-24 10:59:33 +0100

    Red: when given a text with a link, should return the visible text with anchor referring to URL

--- src/tests/markdownTransformer.test.ts
+++ src/tests/markdownTransformer.test.ts
@@ -1,6 +1,6 @@
 /*
     - The markdown transformer when given a text without links, should return the same text.
-    - The markdown transformer when given a text with a link, should return the same text if ePub/Pdf output.
+    - The markdown transformer when given a text with a link, when given a text with a link, should return the visible text with anchor refering to URL.
     - The markdown transformer when given a text with two different links, should return the same text if ePub/Pdf output.
     - The markdown transformer when given a text with a link, should return the text with link using anchor annotation and a page footer with a reference to the anchor pointing to URL if paper output.
     - The markdown transformer when given a text with a two different links, should return the text with links using anchor annotations and two anchor entries in page footer pointing to each URL.
@@ -20,6 +20,16 @@ describe('The Markdown Transformer..', () => {
         const result = markdownTransformer.transform(someText);
 
         expect(result).toBe(someText);    
+    });
+
+    it('when given a text with a link, should return the visible text with anchor referring to URL.', () => {
+        const someText = '[This book](http://codigosostenible.com)';
+        const expectedTransformedText = 'This book[^anchor1]\r\n[^anchor1]: http://codigosostenible.com'
+        const markdownTransformer = new MarkdownTransformer();
+        
+        const result = markdownTransformer.transform(expectedTransformedText);
+
+        expect(result).toBe(someText);
     });
 });
 
